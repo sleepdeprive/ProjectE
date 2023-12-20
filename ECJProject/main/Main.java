884ed51f7966c3e9ed1c0624246be7f4395bc0e6
1100072241 <1100072241@L829WD210013.edu.cbe.ab.ca>
2023-12-20 14:52:10 -0700

    test commit

--- ECJProject/main/Main.java
+++ ECJProject/main/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
+		//test
 		ShellAnimation animation = new ShellAnimation();
 		AnimationFrame frame = new AnimationFrame((Animation)animation);
 		frame.start();
